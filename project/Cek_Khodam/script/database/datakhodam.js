b6c983769c748fab7b38042a7d0fd20481823ad2
Zach Noland <muzackyulum@gmail.com>
2025-04-05 16:36:10 +0700

    New Khodam Game

---
+++ project/Cek_Khodam/script/database/datakhodam.js
@@ -0,0 +1,26 @@
+const anomali_khodam = [
+    "Burung Puyuh",
+    "Hariamu tutul",
+    "Lorem Ipsum dolor sit amet",
+    "Karpet Terbang",
+    "Pintu ajaib",
+    "Rompi Peledak",
+    "C++",
+    "Assembly",
+    "Python",
+    "Anomali Tung-tung Sahur",
+    "Mas-Mas Ambatukam",
+    "Little Boy(Bom Nuklir)",
+    "Mafia Sholawat",
+    "Laptop",
+    "Anomali Hitam",
+    "Skibidi Gman",
+    "Herobrine",
+    "Boboiboy Geleksi",
+    "Guling Anime",
+    "HTML",
+    "Windows XP",
+    "Makananmu Kemarin"
+]
+
+export default anomali_khodam ;
---
+++ project/Cek_Khodam/script/script.js
@@ -0,0 +1,30 @@
+import { RandomNumber } from "./mathformula/setmath.js";
+import anomali_khodam from "./database/datakhodam.js";"./database/datakhodam.js";
+
+let getinputress = document.getElementById("btn_khodam");
+let getinputclear = document.getElementById("btn2_khodam");
+
+document.getElementById("total_khodam").innerText = "Total Khodam Database: " + anomali_khodam.length;
+
+getinputress.onclick= function(){
+    let getusername = document.getElementById("username");
+
+    if (getusername.value) {
+        let number = RandomNumber(0, (anomali_khodam.length - 1));
+        let khodam = anomali_khodam[number];
+        let resultskhodam = `🃏 khodam ${getusername.value} adalah ${khodam}. 🃏`;
+    
+        document.getElementById("setinfo_results").innerHTML = "🔽 Pickup Card Khodam 🔽";
+        document.getElementById("results_khodam").innerHTML = resultskhodam;
+    } else {
+        document.getElementById("results_khodam").innerHTML = "Nama Wajib di isi";
+
+    }
+}
+
+getinputclear.onclick = function() {
+    let getusername = document.getElementById("username");
+
+    document.getElementById("results_khodam").innerHTML = null;
+    document.getElementById("setinfo_results").innerHTML = null;
+}
