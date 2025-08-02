6254079f1323827e84bbc225bf2f5b82759d7acf
Zach Noland <muzackyulum@gmail.com>
2025-08-02 15:25:26 +0700

    Update Visiter interface

---
+++ Javascript/script.js
@@ -0,0 +1,24 @@
+const visitTo = (btn, target) => {
+    btn.addEventListener('click', () => {
+        window.location.href = target;
+    });
+};
+
+const globalVisiter = (btnWeb) => {
+    visitTo(btnWeb[0], "Project/JPCSIM/index.html");
+    visitTo(btnWeb[1], "Project/Value_Input/src/html/index.html");
+    visitTo(btnWeb[2], "Project/Cek_Khodam/html/index.html");
+    visitTo(btnWeb[3], "#");
+};
+
+// Main Code
+document.addEventListener("DOMContentLoaded", () => {
+    const btnGoWeb = [
+        document.getElementById('btnWeb1'),
+        document.getElementById('btnWeb2'),
+        document.getElementById('btnWeb3'),
+        document.getElementById('btnWeb4')
+    ];
+
+    globalVisiter(btnGoWeb);
+});
